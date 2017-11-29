16f892d91d762bcd5e670895a03d895956658e95
DismalAmoeba <brawler721@gmail.com>
2017-11-29 17:59:47 -0600

    Update commonPassMap.java

--- commonPassMap.java
+++ commonPassMap.java
@@ -5,7 +5,8 @@ public class commonPassMap {
 	
 	public static void checkPassword() {
 		try {
-		FileInputStream in = new FileInputStream("/PasswordCracker/src/CommonPasswords.txt"); 
+		FileInputStream in = new FileInputStream("/PasswordCracker/src/CommonPasswords.txt");
+		//"/PasswordCracker/src/CommonPasswords.txt" will need to change depending on what IDE you're using! We probably should decide on an IDE to use.
 		BufferedReader scanner = new BufferedReader(new InputStreamReader(in));
         HashMap<String, String> map = new HashMap<String, String>();
 
